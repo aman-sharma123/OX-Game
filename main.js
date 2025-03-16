ce827c24980e20c9c775d3eda0895ca675430512
aman-sharma123 <amanshpq123@gmail.com>
2025-03-16 20:42:12 +0530

    Update

--- main.js
+++ main.js
@@ -24,5 +24,22 @@ boxes.forEach((box) => {
             box.innerText = "X";
             turnO = true;
         }
+        box.disabled = true;
+        checkWinner();
     });
 });
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if(pos1Val == pos2Val && pos2Val == pos3Val) {
+                console.log(`${pos1Val} wins`);
+            }
+            
+        }
+    }
+}
